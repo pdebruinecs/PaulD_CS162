47e787a2843f29a55acc5c467ab0b207a4205051
brookhamilton <bhamilton3264@gmail.com>
2024-04-05 11:19:22 -0700

    https://github.com/brookham/GitHub-assignment.git

--- main.py
+++ main.py
@@ -2,20 +2,22 @@
 
 # Project requires TWO functions:
 # 1. rect_area (length, width) which will return the area of a rectangle
+def rect_area(length, width):
+    return length * width
 # 2. rect_solid_area (length, width, height) which will return the area of a solid rectangular object
-
+def rect_solid_area(length, width, height):
+    return length * width * height
 # The following four lines are just there to make the code work without errors until functions are added
-def rect_solid_area(x, y, z):
-    return 1
-length = 1; width = 1; height = 1
-rect_solid_area (length, width, height)
 
 # Request the dimension of a solid rectangular object
 length = int(input("Enter the length of the the object as in integer: "))
 width = int(input("Enter the width of the the object as in integer: "))
 height = int(input("Enter the height of the the object as in integer: "))
 
-surface_area = rect_solid_area(length, width, height)
+def surface_area(length, width, height):
+    return 2 * ((length * width) + (width * height) + (length * height))
 
 print("Length = ", length, "Width = ", width, "Height = ", height)
-print("Total Surface Area = ", surface_area)
+print("Total Surface Area = ", str(surface_area(length, width, height)))
+print("Area of the rectangle: " + str(rect_area(length, width)))
+print("Solid Area of the rectangle: " + str(rect_solid_area(length, width, height)))
