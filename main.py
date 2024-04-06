c97d84440e3ff86b914d94a5e7b42f18fa2f875f
James Edward Cagney <45402150+jamescagney@users.noreply.github.com>
2024-04-05 17:09:39 -0700

    Create main.py
    Redid file after accidental pushes

---
+++ main.py
@@ -0,0 +1,19 @@
+# CS 162
+
+# Project requires TWO functions:
+
+# Returns Area of Rectangle
+
+# Returns Surface Area of Rectangular Solid
+
+# Returns Solid Area of Rectangle
+
+# Request the dimension of a solid rectangular object
+length = int(input("Enter the length of the the object as a integer: "))
+width = int(input("Enter the width of the the object as a integer: "))
+height = int(input("Enter the height of the the object as a integer: "))
+
+print("Length = ", length, "Width = ", width, "Height = ", height)
+print("Total Surface Area = ", str(surface_area(length, width, height)))
+print("Area of the rectangle: " + str(rect_area(length, width)))
+print("Solid Area of the rectangle: " + str(rect_solid_area(length, width, height)))
