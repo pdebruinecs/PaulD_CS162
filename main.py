2f6d6eef4157112a40bb304cfc9430b47ab266d9
pdebruinecs <pdebruine@cocc.edu>
2024-04-07 15:11:10 -0700

    git remote add origin https://github.com/Lchance2/LChance-Git-Assignment.git git branch -M main git push -u origin main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #Function 1
 # Returns Area of Rectangle
+
 #Function 2
 # Returns Surface Area of Rectangular Solid
-
 # Request the dimension of a solid rectangular object
+
 length = int(input("Enter the length of the the object as a integer: "))
 width = int(input("Enter the width of the the object as a integer: "))
 height = int(input("Enter the height of the the object as a integer: "))
