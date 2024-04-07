b9a90ad93aa8ed0d7a03e6d987cead12a6858f95
James Edward Cagney <45402150+jamescagney@users.noreply.github.com>
2024-04-07 16:03:56 -0700

    Delete pauldebruine.py
    wrong

--- pauldebruine.py
+++
@@ -1,8 +0,0 @@
-def rect_area(length, width):
-    area = length * width
-    return area
-
-def rect_surface_area(length, width, height):
-    lw = rect_area(length,width)
-    return (lw * height)
-
