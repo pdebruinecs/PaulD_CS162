b220102a4e20a908f05367c4d3f6bb1b1925c0cd
James Edward Cagney <45402150+jamescagney@users.noreply.github.com>
2024-04-07 06:41:43 -0700

    Create main.py
    rebuilt 3rd time

---
+++ main.py
@@ -0,0 +1,13 @@
+#Function 1
+# Returns Area of Rectangle
+#Function 2
+# Returns Surface Area of Rectangular Solid
+
+# Request the dimension of a solid rectangular object
+length = int(input("Enter the length of the the object as a integer: "))
+width = int(input("Enter the width of the the object as a integer: "))
+height = int(input("Enter the height of the the object as a integer: "))
+
+print("Length = ", length, "Width = ", width, "Height = ", height)
+print("Total Surface Area = ", str(rect_surface_area(length, width, height)))
+print("Area of the rectangle: " + str(rect_area(length, width)))
